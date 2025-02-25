8926b7452927eb2c537543d8aadfabd6554d12d9
j-rivs <jrivs15@terpmail.umd.edu>
2025-02-24 22:39:22 -0500

    feat: add initial welcome view

--- ggBase/gg_site_app/views.py
+++ ggBase/gg_site_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# create a function
+def gg_view(request):
+
+    return HttpResponse("<h1>Welcome to GG</h1>")
