395c5887f5451234c32e76fa4fc8a22934b9c1c6
j-rivs <jrivs15@terpmail.umd.edu>
2025-02-24 23:08:18 -0500

    fix: Adjust INSTALLED_APPS

--- ggBase/ggBase/settings.py
+++ ggBase/ggBase/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "gg_site_app.apps.GgSiteAppConfig",
+    "gg_site_app",
 ]
 
 MIDDLEWARE = [
